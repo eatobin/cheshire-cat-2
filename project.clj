32ce200a3362248f3335f2f1986d9b369e04c862
Eric Tobin <eatobin@gmail.com>
2018-04-24 18:57:46 -0500

    progress to pg 120ish

--- project.clj
+++ project.clj
@@ -5,9 +5,18 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
                  [ring/ring-defaults "0.2.1"]
-                 [ring/ring-json "0.4.0"]]
-  :plugins [[lein-ring "0.9.7"]]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/clojurescript "1.10.238"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-cljsbuild "1.1.7"]]
   :ring {:handler cheshire-cat-2.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]]}}
+  :cljsbuild {
+              :builds [{
+                        :source-paths ["src-cljs"]
+                        :compiler     {
+                                       :output-to     "resources/public/main.js"
+                                       :optimizations :whitespace
+                                       :pretty-print  true}}]})
---
+++ src-cljs/cheshire_cat_2/core.cljs
@@ -0,0 +1,4 @@
+(ns cheshire-cat-2.core)
+
+(defn ^:export init []
+  (js/alert "This is fun!"))
