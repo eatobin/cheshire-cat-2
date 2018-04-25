47c047ffaaea781ce93fbcbc828855b5bb42cdb1
Eric Tobin <eatobin@gmail.com>
2018-04-25 17:47:28 -0500

    to pg 131

--- src-cljs/cheshire_cat_2/core.cljs
+++ src-cljs/cheshire_cat_2/core.cljs
@@ -10,5 +10,7 @@
   (go
     (let [response (<! (http/get "/cheshire-cat"))
           body (:body response)]
-      (ef/at "#cat-name" (ef/content (:name body)))
-      (ef/at "#status" (ef/content (:status body))))))
+      (ef/at "#cat-name" (ef/content (:name body))
+             "#status" (ef/do->
+                         (ef/content (:status body))
+                         (ef/set-style :font-size "500%"))))))
